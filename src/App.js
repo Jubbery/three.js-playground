32bcf6803db4098ee8efc84bc9d5b4fee6f1dc76
Jubbery <j.roberts28077@gmail.com>
2023-04-10 10:02:58 -0400

    Added Singularity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import BoxOfHoles from './components/BoxOfHoles';
+import Singularity from './components/Singularity';
 import SphereOfHoles from './components/SphereOfHoles';
 import SphereOfHolesCannon from './components/SphereOfHolesCannon';
 import ThreeScene from './components/ThreeScene';
@@ -10,7 +11,8 @@ function App() {
       {/* <ThreeScene /> */}
       {/* <SphereOfHoles/> */}
       {/* <BoxOfHoles /> */}
-      <SphereOfHolesCannon />
+      {/* <SphereOfHolesCannon /> */}
+      <Singularity />
     </div>
   );
 }
